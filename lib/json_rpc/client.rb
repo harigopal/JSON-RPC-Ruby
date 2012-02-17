7ef2ee93e91fbbd9acfd48218ed38f77908f7517
Hari Gopal <harigopal1@gmail.com>
2012-02-17 16:19:34 +0530

    Playing around with making POST request to server. Untested code here.

--- lib/json_rpc/client.rb
+++ lib/json_rpc/client.rb
@@ -6,6 +6,34 @@ class JsonRpc::Client
   end
 
   def method_missing(method_name, *arguments)
-    Net::HTTP.post_form(@server_url, {'json_rpc' => "2.0", "method" => method_name.to_s, "id" => "a1b2c3"})
+    request_parameters = {
+      "json_rpc" => "2.0",
+      "id" => random_id,
+      "method" => method_name.to_s
+    }
+
+    unless arguments.empty?
+      request_parameters.merge!({
+        "params" => arguments
+      })
+    end
+
+    uri = URI @server_url
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = request_parameters.to_json
+    p req.body
+    req.content_type = "application/json"
+
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    p response.body
+  end
+
+  # Generates a random 10-digit alphanumeric string.
+  def random_id
+    elements = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map {|i| i.to_a}.flatten
+    (0..9).map { elements[rand(elements.length)] }.join
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -12,12 +12,37 @@ describe JsonRpc::Client do
     }.to_not raise_error
   end
 
-  subject { JsonRpc::Client.new('http://example.com') }
+  subject { JsonRpc::Client.new('http://example.com/') }
 
   describe "#method_missing" do
+    before :each do
+      subject.stub(:random_id).and_return("a1b2c3d4e5")
+    end
+
     it "posts JSON RPC request to URL supplied at initialization" do
-      Net::HTTP.should_receive(:post_form).with("http://example.com", {'json_rpc' => "2.0", "method" => "anything", "id" => "a1b2c3"})
+      #Net::HTTP.any_instance.should_receive(:post) #.with("http://example.com", {'json_rpc' => "2.0", "method" => "anything", "id" => "a1b2c3d4e5"})
       subject.anything
+    end
+
+    context "when arguments are supplied" do
+      it "passes along arguments as well" do
+        #Net::HTTP.any_instance.should_receive(:post) #.with("http://example.com", {'json_rpc' => "2.0", "method" => "anything", "id" => "a1b2c3d4e5", "params" => [1, "two", "3"]})
+        subject.anything(1, :two, "3")
+      end
+    end
+  end
+
+  describe "#random_id" do
+    it "generates a string" do
+      subject.random_id.should be_a String
+    end
+
+    it "has length 8" do
+      subject.random_id.length.should == 10
+    end
+
+    it "generates random value" do
+      subject.random_id.should_not == subject.random_id
     end
   end
 end
